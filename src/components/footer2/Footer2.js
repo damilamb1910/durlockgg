b350e8d1dc366af66e04cf507c1d75d392cdd977
Damian Lamberti <damilamb1910@gmail.com>
2024-01-30 20:37:57 -0300

    email confirmacion

--- src/components/footer2/Footer2.js
+++ src/components/footer2/Footer2.js
@@ -29,19 +29,19 @@ const Footer2 = () => {
 <div className='formulario__container'>
     <h4>¡Contactanos!</h4>
     <hr />
-<Form>
+<Form action="https://formsubmit.co/contacto@durlockgg.com" method="POST"> 
 <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Nombre</Form.Label>
-        <Form.Control type="text" placeholder="Escriba aquí su nombre..." />
+        <Form.Control name='name' type="text" placeholder="Escriba aquí su nombre..." />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Escriba aquí su email..." />
+        <Form.Control name='email' type="email" placeholder="Escriba aquí su email..." />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" >
         <Form.Label>Mensaje</Form.Label>
-        <Form.Control as="textarea" rows={3} placeholder="Escriba aquí su mensaje..." />
+        <Form.Control name='mensaje' as="textarea" rows={3} placeholder="Escriba aquí su mensaje..." />
       </Form.Group>
       <Button variant="primary" type="submit">
         Enviar
@@ -88,7 +88,7 @@ const Footer2 = () => {
 
 </div>
 
-<div id='instagram' onClick={()=>{window.open('https://www.instagram.com/damianlamberti/', '_blank')}} className='logo__redes'>
+<div id='instagram' onClick={()=>{window.open('https://www.instagram.com/durlock.gg?igsh=d3pqaDExczM4ZXox&utm_source=qr', '_blank')}} className='logo__redes'>
 
 
 <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
